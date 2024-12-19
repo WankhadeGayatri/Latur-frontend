dfc263726c95fefa078ddeaa28c18e7c0b5dac26
GayatriAWankhade <gayatri01wankhade@gmail.com>
2024-12-19 12:49:34 +0530

    eighth change

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,12 @@ module.exports = {
   testResultsProcessor: "jest-junit",
   preset: 'ts-jest', // Stick with ts-jest for TypeScript (optional, based on your project setup)
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['D:/frontend/hostel-project-webUI/src/setupTests.js'], // Ensure this path is correct
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], // Ensure this path is correct
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',  // This should be sufficient
   },
   moduleNameMapper: {
-    '^@/(.*)$': 'D:/frontend/hostel-project-webUI/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy', // Handle CSS imports
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js', // Handle image imports
   },
